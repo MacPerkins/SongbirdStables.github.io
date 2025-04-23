f7b2439210b90b17081a15b3c218143ba2d92d22
Matthew <macdperkins@gmail.com>
2025-04-23 16:50:46 -0600

    edited home page to include additional horses

--- songbirdstables-site/src/app/page.tsx
+++ songbirdstables-site/src/app/page.tsx
@@ -8,12 +8,9 @@ import Footer from "./components/footer";
 import Zoe from "/src/public/images/zoe.jpg";
 import Caspian from "/src/public/images/caspian.jpg";
 import Dawn from "/src/public/images/dawn.jpg";
+import Aurora from "/src/public/images/aurora.jpg";
+import Juliet from "/src/public/images/juliet.jpg";
 import Hero from "/src/public/images/hero-image.jpg";
-
-// export const metadata: Metadata = {
-//   title: "Horse Riding Lessons",
-//   description: "Professional horse riding lessons",
-// };
 
 export default function Home() {
   // State to manage modal visibility and image details
@@ -51,10 +48,10 @@ export default function Home() {
             Professional horse riding lessons for all ages and skill levels.
           </p>
           <a
-            href="/contact"
-            className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 transition"
+            href="/lessons"
+            className="bg-blue-500 text-2xl text-white py-3 px-7 rounded-lg hover:bg-blue-600 transition"
           >
-            Contact Us
+            Lesson Pricing
           </a>
         </div>
       </section>
@@ -63,16 +60,18 @@ export default function Home() {
       <h2 className={`${birthstone.variable} text-5xl font-semibold text-center mb-6 mt-6`}>
         Meet our lesson Horses!
       </h2>
-      <section className="grid grid-cols-1 sm:grid-cols-3 gap-8 px-8 py-16 bg-gray-100 rounded-t-xl">
+      <section className="flex flex-col gap-8 px-8 py-16 bg-gray-100 rounded-t-xl">
         {/* Horse Boxes */}
         {[
-          { src: Caspian, alt: "Caspian", name: "Caspian" },
-          { src: Dawn, alt: "Dawn", name: "Dawn" },
-          { src: Zoe, alt: "Zoe", name: "Zoe" },
+          { src: Caspian, alt: "Caspian", name: "Caspian", description: "Caspian is our big teddy bear of a horse. Not only does he have gorgeous coloring, but he loves attention. He gets slightly worried about his riders, and beceause of this he is a more intermediate horse. He does well with more beginner riders that are more calm and not anxious when riding." },
+          { src: Dawn, alt: "Dawn", name: "Dawn", description: "Dawn is our majestic girl. She loves attention and snuggles and will follow you around once she gets to know you. With a lively spirit, she loves to walk quickly and is fun to ride for all levels of riding." },
+          { src: Zoe, alt: "Zoe", name: "Zoe", description: "Zoe is our sweet but spunky mustang. With her beautiful paint coat, Zoe loves and does great with kids, and is perfect for all levels of riders." },
+          { src: Aurora, alt: "Aurora", name: "Aurora", description: "Aurora is our youngest horse here and is still in training. She is so sweet, and walks up to greet everyone that visits. She loves kids and is very gentle with her riders. She is used for young and beginner lessons and some advanced lessons." },
+          { src: Juliet, alt: "Juliet", name: "Juliet", description: "Juliet is our dear momma horse. She has a cute baby right now that she is taking care of and will join lessons this fall/winter once her baby gets old enough." },
         ].map((horse) => (
           <div
             key={horse.name}
-            className="flex flex-col items-center p-4 bg-white border border-gray-200 rounded-lg shadow-md w-full h-[300px] cursor-pointer"
+            className="flex flex-col sm:flex-row items-center p-4 bg-white border border-gray-200 rounded-lg shadow-md w-full cursor-pointer"
             onClick={() => openModal(horse.src)}
           >
             <Image
@@ -80,11 +79,16 @@ export default function Home() {
               alt={horse.alt}
               width={200}
               height={200}
-              className="object-cover rounded-xl border border-transparent hover:border-black hover:shadow-xl mb-4"
+              className="object-cover rounded-xl border border-transparent hover:border-black hover:shadow-xl mb-4 sm:mb-0 sm:mr-4"
             />
-            <h2 className={`${birthstone.variable} text-4xl font-semibold`}>
-              {horse.name}
-            </h2>
+            <div className="flex flex-col">
+              <h2 className={`${birthstone.variable} text-4xl font-semibold mb-2`}>
+                {horse.name}
+              </h2>
+              <p className={`${itim.variable} text-lg`}>
+                {horse.description}
+              </p>
+            </div>
           </div>
         ))}
       </section>
