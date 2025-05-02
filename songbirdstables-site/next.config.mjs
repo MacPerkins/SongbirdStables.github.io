c739f7ef31ae8b4beab82db7941958c885cc17a7
Matthew <macdperkins@gmail.com>
2025-05-02 13:16:52 -0600

    removed github pages config

--- songbirdstables-site/next.config.mjs
+++ songbirdstables-site/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/SongbirdStables.github.io/songbirdstables-site",
-  assetPrefix: "/SongbirdStables.github.io/songbirdstables-site",
+  // Remove basePath and assetPrefix for Vercel
 };
 
 export default nextConfig;
